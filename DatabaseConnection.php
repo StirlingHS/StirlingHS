96ea1005e919260fc8f90db0f5c8e59883af308e
Ryke1410 <32765187+Ryke1410@users.noreply.github.com>
2017-10-13 11:34:10 +0000

    Database Connection - Screb

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -2,7 +2,7 @@
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'contactz';
+$db_name = 'screb';
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {
 	die ('Failed to connect to MySQL Database:'. mysqli_connect_error());
