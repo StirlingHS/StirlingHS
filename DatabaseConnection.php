db0bca896b2aa6a218055dbe1e22ec2929015bbf
Malumdrome7102 <32765069+Malumdrome7102@users.noreply.github.com>
2017-10-13 11:27:44 +0100

    Contacts
    z

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -2,7 +2,7 @@
 $db_host = 'localhost';
 $db_user = 'root';
 $db_pass = '';
-$db_name = 'contacts';
+$db_name = 'contactz';
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {
 	die ('Failed to connect to MySQL Database:'. mysqli_connect_error());
